e1c04a75c9758f2f32ed0a84f40e49351aa3b0ea
Daniel Munusami <daniel.munusami@gmail.com>
2016-04-08 13:03:22 -0400

    changes to VerifyBattleWin
    makes more sense now

--- DurakGame/Rules/VerifyBattleWin.cs
+++ DurakGame/Rules/VerifyBattleWin.cs
@@ -26,7 +26,7 @@ namespace DurakGame.Rules
 
         public void ValidateState(PlayerCollection players, GameState currentState)
         {
-            if (currentState.GetValueCardSuit("defending_card_suit") > currentState.GetValueCardSuit("attacking_card_suit"))
+            if (currentState.GetValueCardSuit("defending_card_suit") == currentState.GetValueCardSuit("attacking_card_suit") && currentState.GetValueCardRank("attacking_card_rank") >= currentState.GetValueCardRank("defending_card_rank"))
             {
                 currentState.Set("current_round", (byte)(currentState.GetValueByte("current_round") + 1));
             }
