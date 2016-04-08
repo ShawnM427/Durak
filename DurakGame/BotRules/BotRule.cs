9c03830d95e0a006327bba572f63b030d512a374
Alex Donnelly <alexandercassius@hotmail.com>
2016-04-08 11:33:34 -0400

    Files moved to bot rule folder

---
+++ DurakGame/BotRules/BotRule.cs
@@ -0,0 +1,16 @@
+﻿using Durak.Server;
+using System;
+using Durak.Common;
+
+namespace DurakTester.Rules
+{
+    public class BotRule : IBotInvokeStateChecker
+    {
+        public bool ShouldInvoke(GameState state, BotPlayer player)
+        {
+            return 
+                (state.GetValueByte("attacking_player_id") == player.Player.PlayerId & state.GetValueBool("IsAttacking")) | 
+                (state.GetValueByte("defending_player_id") == player.Player.PlayerId & !state.GetValueBool("IsAttacking"));
+        }
+    }
+}
